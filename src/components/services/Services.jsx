7a9e89cec87714a9fb6011d45fc8e7d119e08f97
Minh Long SE <longbo1208@gmail.com>
2023-10-27 22:55:40 +0700

    fix ui

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,20 +1,63 @@
-import "./services.scss"
+import "./services.scss";
 import { motion } from "framer-motion";
 const Services = () => {
   return (
-    <motion.div className='services'>
-        <motion.div className="text-container">
-            <p>
-                I focus on helping your brand grow
-                <br/> and move forward
-            </p>
-            <hr/>
-        </motion.div>
-        <motion.div className="title-Container"></motion.div>
-        <motion.div className="list-Container"></motion.div>
-
+    <motion.div className="services">
+      <motion.div className="text-container">
+        <p>
+          I focus on helping your brand grow
+          <br /> and move forward
+        </p>
+        <hr />
+      </motion.div>
+      <motion.div className="title-Container">
+        <div className="title">
+          <img src="/people.webp" alt="" />
+          <h1>
+            <b>Unique</b>Ideas
+          </h1>
+        </div>
+        <div className="title">
+          <h1>
+            <b>For you</b> Business.
+          </h1>
+          <button>WHAT WE DO ?</button>
+        </div>
+      </motion.div>
+      <motion.div className="list-Container">
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ullam
+            dicta reiciendis. Laboriosam ducimus cumque cupiditate laborum.
+            Voluptas repellat illo deserunt aperiam quasi nostrum, nisi, animi
+            rem nemo itaque veniam.
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ullam
+            dicta reiciendis. Laboriosam ducimus cumque cupiditate laborum.
+            Voluptas repellat illo deserunt aperiam quasi nostrum, nisi, animi
+            rem nemo itaque veniam.
+          </p>
+          <button>Go</button>
+        </div>
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde ullam
+            dicta reiciendis. Laboriosam ducimus cumque cupiditate laborum.
+            Voluptas repellat illo deserunt aperiam quasi nostrum, nisi, animi
+            rem nemo itaque veniam.
+          </p>
+          <button>Go</button>
+        </div>
+      </motion.div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
